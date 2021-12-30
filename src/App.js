825defdc72ad04304ecd7624f3d4e72bf843eb1c
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2021-12-30 16:15:16 +0100

    Done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import CounterComponent from "./counterContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <CounterComponent />
+    </div>
+  );
+}
+
+export default App;
+ 
